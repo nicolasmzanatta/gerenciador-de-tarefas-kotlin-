7f5a506b13da09813e99d17f4e0bd6336702ad5e
Nicolas Moreira Zanatta <nicolas.mzanatta93@gmail.com>
2021-06-18 00:34:52 -0300

    api de tarefas finalizada kotlin

--- src/main/kotlin/br/com/devaria/projetos/gerenciadortarefas/models/Usuario.kt
+++ src/main/kotlin/br/com/devaria/projetos/gerenciadortarefas/models/Usuario.kt
@@ -1,12 +1,19 @@
+
 package br.com.devaria.projetos.gerenciadortarefas.models
 
+import com.fasterxml.jackson.annotation.JsonBackReference
 import javax.persistence.Entity
 import javax.persistence.*
 
 @Entity
 data class Usuario (
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0,
-    val nome: String = "",
-    val email: String = "",
-    var senha: String = "")
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long = 0,
+        val nome: String = "",
+        val email: String = "",
+        var senha: String = "",
+
+        @JsonBackReference
+        @OneToMany(mappedBy = "usuario", fetch = FetchType.EAGER)
+        val tarefas : List<Tarefa> = emptyList()
+)
